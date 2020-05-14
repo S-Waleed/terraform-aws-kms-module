68bb6571018a6f31a068ea2d33edc265432421d8
Waleed <masterwali@Waleeds-MacBook-Pro.local>
2020-05-13 22:48:20 -0400

    password policy

--- main.tf
+++ main.tf
@@ -13,13 +13,22 @@ resource "aws_iam_account_alias" "this" {
   account_alias = var.aws_account_alias
 }
 
-# deactivate endpoints
-
 # password policy
+resource "aws_iam_account_password_policy" "this" {
+  minimum_password_length        = 14
+  max_password_age               = 150
+  password_reuse_prevention      = 3
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
 
 # s3 settings
 
 # ec2 volume encryption
 
 # cloud trail settings
+# deactivate endpoints
 
