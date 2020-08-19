66d13d2f732ed308e6f3dace0a972ebe616f4030
Waleed <masterwali@Waleeds-MacBook-Pro.local>
2020-08-19 06:38:11 -0400

    removed provider

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_cli_profile
-}
-
-terraform {
-  # The configuration for this backend will be
-  # filled in by Terragrunt
-  backend "s3" {}
-}
-
 # Creates/manages KMS CMK
 resource "aws_kms_key" "this" {
   description              = var.description
--- vars.tf
+++ vars.tf
@@ -1,9 +1,3 @@
-variable aws_region {
-  default = "us-east-1"
-}
-
-variable aws_cli_profile {}
-
 variable description {}
 
 # Options available
